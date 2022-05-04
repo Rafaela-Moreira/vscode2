a3353e8466e2bb29d2b76ba32691bf6e58afd5af
Rafaela Moreira <86133113+Rafaela-Moreira@users.noreply.github.com>
2022-05-04 20:44:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+var { usuario } = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get("/", async function(req, res){
+  var usuarios = await usuario.findAll();
+  res.json(usuarios);
+});
+
+app.listen(3000, function() {
+  console.log(
+    "O servidor funciona");
+});
+
+app.post("/", async function(req, res){
+var resultado = await usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/:id", async function(req, res){
+var resultado = await usuario.update(
+  {
+    nome: req.body.nome,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  }
+);
+
+res.json(resultado);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
