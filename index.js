34a0100f69bb320d91c28abae808a428a6468a2d
Rafaela Moreira <86133113+Rafaela-Moreira@users.noreply.github.com>
2022-05-11 21:17:37 +0000

    adicionamos o modelo empresa e relacionamento com o usuario

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
-var { usuario } = require("./models");
+var { usuario, empresa } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res){
+app.get("/usuarios", async function(req, res){
   var usuarios = await usuario.findAll();
   res.json(usuarios);
 });
@@ -17,12 +17,12 @@ app.listen(3000, function() {
     "O servidor funciona");
 });
 
-app.post("/", async function(req, res){
+app.post("/usuarios", async function(req, res){
 var resultado = await usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function(req, res){
+app.put("/usuarios/:id", async function(req, res){
 var resultado = await usuario.update(
   {
     nome: req.body.nome,
@@ -37,7 +37,7 @@ var resultado = await usuario.update(
 res.json(resultado);
 });
 
-app.delete("/:id", async function(req, res){
+app.delete("/usuarios/:id", async function(req, res){
 var resultado = await usuario.destroy({
     where: {
       id: req.params.id,
@@ -46,7 +46,46 @@ var resultado = await usuario.destroy({
   res.json(resultado);
 });
 
-app.get("/:id", async function(req, res){
+app.get("/usuarios/:id", async function(req, res){
   var usuarios = await usuario.findByApk(req.params.id);
   res.json(usuarios);
+});
+
+app.get("/empresas", async function(req, res){
+  var empresas = await empresa.findAll();
+  res.json(empresas);
+});
+
+app.post("/empresas", async function(req, res){
+var resultado = await empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", async function(req, res){
+var resultado = await empresa.update(
+  {
+    nome: req.body.nome,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  }
+);
+
+res.json(resultado);
+});
+
+app.delete("/empresas/:id", async function(req, res){
+var resultado = await empresa.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(resultado);
+});
+
+app.get("/empresas/:id", async function(req, res){
+  var empresas = await empresa.findByApk(req.params.id);
+  res.json(empresas);
 });
